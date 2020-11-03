88488cfff1b102f79c64d5c6a18ca6e4d67cb603
Owen Melia <owenjmelia@gmail.com>
2020-11-03 12:09:37 -0600

    Ready to run on TTIC

---
+++ scripts/run_Burgers.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name=run_Burgers
+#SBATCH --time=24:00:00
+#SBATCH --partition=contrib-gpu-long
+#SBATCH --output=logs/run_Burgers.out
+#SBATCH --error=logs/run_Burgers.err
+
+
+mkdir -p ~/projects/PINNs/figures
+mkdir -p ~/projects/PINNs/tables
+
+source ~/conda_init.sh
+
+conda activate emulators_tf115
+
+python appendix/continuous_time_identification\ \(Burgers\)/Burgers.py
---
+++ scripts/run_Burgers_systematic.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name=run_systematic
+#SBATCH --time=24:00:00
+#SBATCH --partition=contrib-gpu-long
+#SBATCH --output=logs/run_systematic.out
+#SBATCH --error=logs/run_systematic.err
+
+
+mkdir -p ~/projects/PINNs/figures
+mkdir -p ~/projects/PINNs/tables
+
+source ~/conda_init.sh
+
+conda activate emulators_tf115
+
+python appendix/continuous_time_identification\ \(Burgers\)/Burgers_systematic.py
